b992ea260de588059ae0b083011d95b2d4419fd6
MatiasManuelCarro <matiasmanuelcarro@gmail.com>
2026-04-03 14:20:31 -0300

    tpi js parte 1

--- 3_JS/js/data.js
+++ 3_JS/js/data.js
@@ -2,20 +2,53 @@
 const categorias = ["Hamburguesas", "Pizzas", "Papas Fritas", "Bebidas"];
 
 const productos = [
-{
-id: 1,
-nombre: "Hamburguesa Triple",
-descripcion: "Triple carne, cheddar y bacon",
-precio: 25000,
-imagen: "https://via.placeholder.com/300x200/ff6347/ffffff?text=Hamburguesa",
-categoria: "Hamburguesas"
-},
-{
-id: 2,
-nombre: "Pizza Muzzarella",
-descripcion: "Salsa casera y orégano",
-precio: 18000,
-imagen: "https://via.placeholder.com/300x200/ff6347/ffffff?text=Pizza",
-categoria: "Pizzas"
-},
+    {
+        id: 1,
+        nombre: "Hamburguesa Triple",
+        descripcion: "Triple carne, cheddar y bacon",
+        precio: 25000,
+        imagen: "/img/hamburguesa.jpg",
+        categoria: "Hamburguesas"
+    },
+    {
+        id: 2,
+        nombre: "Pizza Muzzarella",
+        descripcion: "Salsa casera y orégano",
+        precio: 18000,
+        imagen: "/img/pizza.jpg",
+        categoria: "Pizzas"
+    },
+    {
+        id: 3,
+        nombre: "Papas Fritas",
+        descripcion: "Porción de papas fritas",
+        precio: 9000,
+        imagen: "/img/papas_fritas.jpg",
+        categoria: "Papa Fritas"
+    },
+        {
+        id: 4,
+        nombre: "Papas Fritas con Cheddar y Panceta",
+        descripcion: "Porción de papas fritas con mucho Cheddar y Panceta",
+        precio: 12000,
+        imagen: "/img/papas_fritas_cheddar.jpg",
+        categoria: "Papa Fritas"
+    },
+    {
+        id: 5,
+        nombre: "Gaseosa",
+        descripcion: "Gaseosa de 500ml",
+        precio: 2500,
+        imagen: "/img/gaseosa.jpg",
+        categoria: "Bebidas"
+    },
+        {
+        id: 6,
+        nombre: "Agua",
+        descripcion: "Agua de 500ml",
+        precio: 1500,
+        imagen: "/img/agua.jpg",
+        categoria: "Bebidas"
+    },
+
 ];
--- 3_JS/js/main.js
+++ 3_JS/js/main.js
@@ -1,11 +1,78 @@
 const listaCategorias = document.getElementById("lista-categorias");
+const contenedorProductos = document.getElementById("contenedor-productos");
+const modal = document.getElementById("modal");
+const cerrarcarrito = document.getElementById("cerrar-carrito");
 
-function cargarCategorias() {
-    categorias.forEach(function (categoria) {
-        const li = document.createElement("li")
-        li.innerHTML = `<a href="3">${categoria}</a>`
-        listaCategorias.appendChild(li)
-    });
+
+cerrarcarrito.onclick = function () {
+    modal.style.display = "none";
+}
+
+
+
+window.onclick = function (event) {
+    const modal = document.getElementById("modal");
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
 };
 
+
+
+const cargarCategorias = () => {
+    categorias.forEach((categoria) => {
+        const li = document.createElement('li')
+        li.innerHTML = `<a href="#">${categoria}</a>`
+        listaCategorias.appendChild(li)
+
+    });
+}
 cargarCategorias();
+
+const cargarProductos = () => {
+    productos.forEach((producto) => {
+        const productosDestacados = document.createElement('article')
+        productosDestacados.classList.add("productosDestacados")
+        productosDestacados.innerHTML = `
+        <img src=${producto.imagen} img alt=${producto.nombre} />
+        <h3>${producto.nombre}</h3>
+        <p>${producto.descripcion}</p>
+        <p>Precio: $${producto.precio}</p>
+        <button class=btn-detalles data-id="${producto.id}">Ver Detalles</button>
+        <button class=btn-carrito data-id="${producto.id}">Agregar al Carrito</button>
+        `
+        contenedorProductos.appendChild(productosDestacados);
+
+
+    });
+}
+cargarProductos();
+
+
+// contenedorProductos.addEventListener("click", (evento) => {
+//     if (evento.target.classList.contains("btn-carrito")) {
+//         const idProducto = evento.target.dataset.id;
+//         const producto = productos.find(p => p.id == idProducto);
+//         // alert(`Se agrega al carrito: ${producto.nombre}`)
+//         const modalCarrito = document.getElementById('modal-carrito');
+//         modalCarrito.innerHTML = `<p>Carrito de Compras</p>
+//         <p>Se agrega al carrito: ${producto.nombre}</p>`
+//         modal.style.display = "block";
+//     }
+
+
+    contenedorProductos.addEventListener("click", (evento) => {
+    if (evento.target.classList.contains("btn-carrito")) {
+        const idProducto = evento.target.dataset.id;
+        const producto = productos.find(p => p.id == idProducto);
+        // alert(`Se agrega al carrito: ${producto.nombre}`)
+        const modalCarrito = document.getElementById('modal-carrito');
+        modalCarrito.innerHTML = `<p>Carrito de Compras</p>
+        <p>Se agrega al carrito: ${producto.nombre}</p>`
+        modal.style.display = "block";
+    }
+
+
+
+});
+
